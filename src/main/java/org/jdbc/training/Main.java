909616275c970af45e59cbd4710967521810880c
i.gunawan <i.gunawan@gdn-commerce.com>
2025-11-26 13:18:11 +0700

    day 3

--- src/main/java/org/jdbc/training/Main.java
+++ src/main/java/org/jdbc/training/Main.java
@@ -2,7 +2,9 @@ package org.jdbc.training;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
   public static void main(String[] args) {
@@ -20,12 +22,19 @@ public class Main {
 
     // Step 3: Create Connection
     try {
-      Connection connection = DriverManager.getConnection(url, "myuser", "mypassword");
+      Connection connection = DriverManager.getConnection(url, "postgres", "password");
       System.out.println("Connected to PostgreSQL database!");
 
       // Use the 'connection' object for database operations
 
+      Statement s = connection.createStatement();
+      ResultSet rs = s.executeQuery("select * from users");
+      while(rs.next()) {
+          System.out.print("Name:"+rs.getString("name")+", ");
+          System.out.println("Age:"+rs.getInt("age"));
+      }
       // Don't forget to close the connection when done
+      rs.close();
       connection.close();
     } catch (SQLException e) {
       System.out.println("Connection to PostgreSQL database failed.");
